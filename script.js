d0b35317bf8bbe3bad68cccd2a624bf346d94b86
Deepanshi <bishtdipanshi569@gmail.com>
2026-04-30 13:36:35 +0530

    local storage added

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const itemArray = [];
+let itemArray = JSON.parse(localStorage.getItem("tasks")) || [];
+
 const taskList = document.querySelector(".task-list");
+const saveToLocalStorage = () => {
+  localStorage.setItem("tasks", JSON.stringify(itemArray));
+};
 
 const renderTask = () => {
   taskList.innerHTML = "";
   itemArray.forEach((item, index) => {
     const list = document.createElement("li");
     list.innerHTML = `
+     
     <input type="checkbox" class="checkTask" ${item.isCompleted ? "checked" : ""} />
   <span class="task-title ${item.isCompleted ? "completed" : ""}">
     ${item.title}
   </span>
         <div class="btn-group">
        <button class="editBtn">Edit</button>
-      <button class="deleteBtn">Delete</button
+      <button class="deleteBtn">Delete</button>
        </div>
         `;
     const editBtn = list.querySelector(".editBtn");
@@ -24,9 +29,9 @@ const renderTask = () => {
     taskList.appendChild(list);
   });
 };
-
 const updateTask = (index, event) => {
   itemArray[index].isCompleted = event.target.checked;
+  saveToLocalStorage();
   renderTask();
 };
 let editIndex = null;
@@ -45,6 +50,18 @@ const addTask = () => {
     alert("please enter value");
     return;
   }
+  if (addValue.length < 3) {
+    alert("Task must be at least 3 characters");
+    return;
+  }
+  if (/\d/.test(addValue)) {
+    alert("Numbers are not allowed in task");
+    return;
+  }
+  if (addValue.length > 30) {
+    alert("Task must be less than 30 characters");
+    return;
+  }
   if (editIndex !== null) {
     itemArray[editIndex].title = addValue;
     editIndex = null;
@@ -56,6 +73,7 @@ const addTask = () => {
     });
   }
   input.value = "";
+  saveToLocalStorage();
   renderTask();
 };
 
@@ -69,5 +87,8 @@ input.addEventListener("keydown", function (event) {
 });
 const deleteTask = (index) => {
   itemArray.splice(index, 1);
+  saveToLocalStorage();
   renderTask();
 };
+
+renderTask();
